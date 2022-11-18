864cd4a6ad59c93b0b6656deb1db7b04232cd285
Christopher Tremblay <christopher.tremblay@epfl.ch>
2022-11-18 14:45:15 +0100

    Phonebook exercices 2.15-2.18

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,55 +1,90 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Form from './Form';
 import Filter from './Filter';
 import Persons from './Persons';
+import personsService from './services/persons';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState({
+    query: '',
+    displayList: null,
+    completeList: null,
+  });
   const [formState, setFormState] = useState({ name: '', phone: '', id: '' });
-  const [filter, setFilter] = useState({ query: '', list: persons });
 
-  const handleChange = (e) => {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
-  };
-
-  const handleFilterChange = (e) => {
-    const results = persons.filter((person) => {
-      if (e.target.value === '') return persons;
-      return person.name.toLowerCase().includes(e.target.value.toLowerCase());
-    });
-    setFilter({
-      query: e.target.value,
-      list: results,
+  const getAndSetAllPersons = async () => {
+    const allPersons = await personsService.getAll();
+    setPersons({
+      query: '',
+      displayList: allPersons,
+      completeList: allPersons,
     });
   };
 
-  const addPerson = (e) => {
+  const addPerson = async (e) => {
     e.preventDefault();
     if (checkUniqueName(formState.name)) {
       const newPerson = {
         name: formState.name,
         phone: formState.phone,
+        id: `${formState.name}`,
       };
-      setPersons(persons.concat(newPerson));
-      setFilter({ query: '', list: persons.concat(newPerson) });
+      const created = await personsService.create(newPerson);
+      console.log(`${created.name} created with id ${created.id}`);
     } else {
-      alert(`${formState.name} already taken`);
+      if (
+        window.confirm(
+          `${formState.name} is already added to the phonebook, do you want to replace the number?`
+        )
+      ) {
+        const toBeModified = await personsService.getById(formState.name);
+        await personsService.update(formState.name, {
+          ...toBeModified,
+          phone: formState.phone,
+        });
+      } else {
+        alert(`${formState.name} already taken`);
+      }
     }
+    getAndSetAllPersons();
     setFormState({ name: '', phone: '', id: '' });
   };
 
-  const checkUniqueName = (name) => !persons.some((el) => el.name === name);
+  const handleDelete = async (id) => {
+    if (window.confirm('Do you really want to remove this user?')) {
+      await personsService.remove(id);
+      getAndSetAllPersons();
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleFilterChange = async (e) => {
+    const results = persons.completeList.filter((person) => {
+      if (e.target.value === '') return persons.completeList;
+      return person.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setPersons({
+      ...persons,
+      query: e.target.value,
+      displayList: results,
+    });
+  };
+
+  const checkUniqueName = (name) =>
+    !persons.completeList.some((el) => el.name === name);
+
+  useEffect(() => {
+    getAndSetAllPersons();
+  }, []);
 
   return (
     <div>
       <h1>Phonebook</h1>
       <h2>Filter your phonebook</h2>
-      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+      <Filter filter={persons} handleFilterChange={handleFilterChange} />
       <h2>Add new number</h2>
       <Form
         form={formState}
@@ -57,7 +92,9 @@ const App = () => {
         handleSubmit={addPerson}
       />
       <h2>Numbers</h2>
-      <Persons persons={filter} />
+      {persons.displayList && (
+        <Persons persons={persons.displayList} handleDelete={handleDelete} />
+      )}
     </div>
   );
 };
--- part2/phonebook/src/Persons.js
+++ part2/phonebook/src/Persons.js
@@ -1,9 +1,10 @@
-const Persons = ({ persons }) => {
+const Persons = ({ persons, handleDelete }) => {
   return (
     <ul>
-      {persons.list.map((person) => (
+      {persons.map((person) => (
         <li key={person.name}>
-          {person.name}: {person.phone}
+          {person.name}: {person.phone}{' '}
+          <button onClick={() => handleDelete(person.id)}>Delete</button>
         </li>
       ))}
     </ul>

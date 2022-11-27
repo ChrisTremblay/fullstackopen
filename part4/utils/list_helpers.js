8ad06041ec6e97900da34afa0a2d08a9afbe35c0
Christopher Tremblay <christopher.tremblay@epfl.ch>
2022-11-27 17:22:33 +0100

    Part4 exercices 4.3 to 4.7

---
+++ part4/utils/list_helpers.js
@@ -0,0 +1,54 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const objectIsEmpty = (object) => {
+  return Object.keys(output).length === 0;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((prev, next) => prev + next.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (prev, next) => (prev.likes > next.likes ? prev : next),
+    {}
+  );
+};
+
+const concatBlogsByAuthors = (blogs) => {
+  return (output = Object.values(
+    blogs.flat().reduce((accumulator, { author, likes }) => {
+      accumulator[author] ??= { author, blogs: 0, likes: 0 };
+      accumulator[author].blogs += 1;
+      accumulator[author].likes += Number(likes);
+      return accumulator;
+    }, {})
+  ));
+};
+
+const returnMostByKey = (blogs, key) => {
+  const most = concatBlogsByAuthors(blogs).reduce(
+    (prev, next) => (prev[key] > next[key] ? prev : next),
+    {}
+  );
+  return objectIsEmpty(most) ? {} : { author: most.author, [key]: most[key] };
+};
+
+const mostBlogs = (blogs) => {
+  return returnMostByKey(blogs, 'blogs');
+};
+
+const mostLikes = (blogs) => {
+  return returnMostByKey(blogs, 'likes');
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+  concatBlogsByAuthors,
+};
